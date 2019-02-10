32e6632360c4b9d75e3bde4f592fb0bc2039b6b6
Marcin Iskra <dunravel@gmail.com>
2019-02-10 11:20:16 +0100

    Added RandomServlet

--- src/main/java/pl/sda/randomservlet/RandomServlet.java
+++ src/main/java/pl/sda/randomservlet/RandomServlet.java
@@ -6,14 +6,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 
-@WebServlet(name = "RandomServlet")
+@WebServlet(name = "RandomServlet", urlPatterns = "/RandomServlet")
 public class RandomServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Random random = new Random();
+        int[] numbers = new int[5];
+        for(int i = 0; i < 5; i++){
+            numbers[i] = random.nextInt(249)+51;
+        }
 
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>\n<body>");
+        writer.println("<ul>");
+        for(int number : numbers){
+            writer.println("<li>"+number+"</li>");
+        }
+        writer.println("</ul>");
+        writer.println("</html>\n</body>");
     }
 }
